2fe904557b8e6018a518e4bd46a2eee2ab77a3d5
Hardy Smith <hardysmith940110@gmail.com>
2023-12-17 12:19:01 -0500

    update: added ChakraUIProviders
    Signed-off-by: Logan Nguyen <logan.nguyen@swirldslabs.com>y

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
+import { ChakraUIProviders } from '@/libs/chakra-ui/providers';
 import '@/styles/globals.css';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Hedro-ai',
@@ -17,7 +15,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body>
+        <ChakraUIProviders>{children}</ChakraUIProviders>
+      </body>
     </html>
   );
 }
