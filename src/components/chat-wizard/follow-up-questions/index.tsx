1a68b45314d8915882fd6ba48835f0eae7de0e4a
Hardy Smith <hardysmith940110@gmail.com>
2023-12-17 21:07:09 -0500

    feat: added FollowUpQuestion component
    Signed-off-by: Logan Nguyen <logan.nguyen@swirldslabs.com>

---
+++ src/components/chat-wizard/follow-up-questions/index.tsx
@@ -0,0 +1,19 @@
+import { FaRegEdit } from 'react-icons/fa';
+
+interface PageProps {
+  question: string;
+}
+
+const FollowUpQuestion = ({ question }: PageProps) => {
+  return (
+    <div className='flex items-center bg-[#fff] border border-bordercolor/100 rounded-lg cursor-pointer hover:-translate-y-0.5 transition ease-in-out px-2 py-1'>
+      <p className='text-secondary font-bold tracking-tight'>{question}</p>
+
+      <div className='text-xl hover:scale-110 pl-2' title='Edit this question'>
+        <FaRegEdit />
+      </div>
+    </div>
+  );
+};
+
+export default FollowUpQuestion;
