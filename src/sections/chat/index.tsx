325863093f348e888f9b377fba750fb5a3e50502
Hardy Smith <hardysmith940110@gmail.com>
2023-12-17 21:08:02 -0500

    update: updated Chat component
    Signed-off-by: Logan Nguyen <logan.nguyen@swirldslabs.com>

--- src/sections/chat/index.tsx
+++ src/sections/chat/index.tsx
@@ -1,12 +1,13 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
 import ChatInputBox from '@/components/chat-input-box';
 import { VerticalCommonVariants } from '@/libs/framer-motion/variants';
 
 const ChatWithHedro = () => {
   const [chatValue, setChatValue] = useState('');
+  const [isChatMode, setIsChatMode] = useState(false);
   const verticalVariant = VerticalCommonVariants(45, 0.5);
 
   const handleOnSubmit = (e: any) => {
@@ -14,37 +15,43 @@ const ChatWithHedro = () => {
     if (chatValue !== '') {
       console.log('meow');
       setChatValue('');
+      setIsChatMode(true);
     }
   };
 
   return (
-    <section className='w-full h-[100vh] flex flex-col gap-9 items-center justify-center'>
+    <section
+      className={`w-[1000px] min-h-[100vh] flex flex-col gap-9 items-center justify-center relative`}
+    >
       <motion.div
         initial='hidden'
         whileInView='show'
         variants={verticalVariant}
-        className='flex flex-col items-center justify-center'
+        className='flex flex-col min-h-[100vh] w-full items-center justify-center'
       >
-        {/* <h1 className='logo-teal-gradient logo-text'>Hedro.ai</h1> */}
         <motion.h1
           variants={verticalVariant}
-          className='logo-teal-gradient logo-text'
+          className={`logo-teal-gradient logo-text`}
         >
           Hedro.ai
         </motion.h1>
-        <motion.h2 variants={verticalVariant} className='sub-logo-text'>
+        <motion.h2
+          variants={verticalVariant}
+          className={`sub-logo-text ${isChatMode && `hidden`}`}
+        >
           Hedera AI Chatbot Assistant - Elevate Efficiency, Ignite Productivity!
         </motion.h2>
 
         <motion.form
           variants={verticalVariant}
           onSubmit={handleOnSubmit}
-          className='w-full'
+          className={`w-full flex justify-center items-center mb-2`}
         >
           <ChatInputBox
             note={true}
             id={'chat-input'}
             value={chatValue}
+            isChatMode={isChatMode}
             handleOnSubmit={handleOnSubmit}
             handleOnFieldChange={(e: any) => {
               setChatValue(e.target.value);
